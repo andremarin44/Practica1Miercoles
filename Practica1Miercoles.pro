6b4c274b3159565a791c8fc27d88ff271fb5070a
Juliana Castaño <andremarin44@gmail.com>
2024-03-01 23:17:07 -0500

    parte del ejercicio 5, problemas tecnicos con el pc

--- Practica1Miercoles.pro
+++ Practica1Miercoles.pro
@@ -7,6 +7,7 @@ SOURCES += \
         Punto2.cpp \
         Punto3.cpp \
         Punto4.cpp \
+        Punto5.cpp \
         Punto6.cpp \
         Punto7.cpp \
         main.cpp
---
+++ Punto5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int Ejercicio5(){
+    int n;
+    char simbolo;
+    cout<<"Ingrese el simbolo que desea: ";
+    cin>>simbolo;
+    cout<<"\n";
+
+    if (n%2==0){
+        n++;
+    }
+    for (int i =1; i <=n ; i+=2){
+
+    }
+
+}
